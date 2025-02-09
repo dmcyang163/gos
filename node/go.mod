e78b571bc2129f5fad9d0987bc72ef129e1b250d
dmcyang163 <dmc_yang@163.com>
2025-02-09 17:48:14 +0000

    ipfs

--- node/go.mod
+++ node/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/fatih/color v1.18.0
 	github.com/golang/snappy v0.0.4
 	github.com/google/uuid v1.6.0
+	github.com/ipfs/go-ipfs-api v0.7.0
 	github.com/panjf2000/ants/v2 v2.11.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.3
@@ -15,8 +16,33 @@ require (
 )
 
 require (
+	github.com/benbjohnson/clock v1.3.0 // indirect
+	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/crackcomm/go-gitignore v0.0.0-20170627025303-887ab5e44cc3 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/ipfs/boxo v0.12.0 // indirect
+	github.com/ipfs/go-cid v0.4.1 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.3 // indirect
+	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
+	github.com/libp2p/go-flow-metrics v0.1.0 // indirect
+	github.com/libp2p/go-libp2p v0.26.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/multiformats/go-base32 v0.1.0 // indirect
+	github.com/multiformats/go-base36 v0.2.0 // indirect
+	github.com/multiformats/go-multiaddr v0.8.0 // indirect
+	github.com/multiformats/go-multibase v0.2.0 // indirect
+	github.com/multiformats/go-multicodec v0.9.0 // indirect
+	github.com/multiformats/go-multihash v0.2.3 // indirect
+	github.com/multiformats/go-multistream v0.4.1 // indirect
+	github.com/multiformats/go-varint v0.0.7 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	lukechampine.com/blake3 v1.1.7 // indirect
 )
---
+++ node/storage/octopus.go
@@ -0,0 +1,85 @@
+package storage
+
+import (
+	"bufio"
+	"fmt"
+
+	shell "github.com/ipfs/go-ipfs-api"
+)
+
+// Octopus 接口定义了与 IPFS 相关的操作
+type Octopus interface {
+	AddFile(filePath string) (string, error)
+	GetFile(cid string) (string, error)
+	UpdateFile(cid, newFilePath string) (string, error)
+	DeleteFile(cid string) error
+}
+
+// octopusImpl 是 Octopus 接口的一个实现
+type octopusImpl struct {
+	shell *shell.Shell
+}
+
+// NewOctopus 创建一个新的 Octopus 实例
+func NewOctopus() Octopus {
+	return &octopusImpl{
+		shell: shell.NewShell("localhost:5001"),
+	}
+}
+
+// AddFile 将文件添加到 IPFS 网络并返回其 CID
+func (o *octopusImpl) AddFile(filePath string) (string, error) {
+	cid, err := o.shell.Add(filePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to add file to IPFS: %v", err)
+	}
+	return cid, nil
+}
+
+// GetFile 根据 CID 从 IPFS 网络获取文件
+func (o *octopusImpl) GetFile(cid string) (string, error) {
+	file, err := o.shell.Cat(cid)
+	if err != nil {
+		return "", fmt.Errorf("failed to get file from IPFS: %v", err)
+	}
+	defer file.Close()
+
+	// 读取文件内容到缓冲区
+	scanner := bufio.NewScanner(file)
+	content := ""
+	for scanner.Scan() {
+		content += scanner.Text() + "\n"
+	}
+
+	if err := scanner.Err(); err != nil {
+		return "", fmt.Errorf("failed to read file content: %v", err)
+	}
+
+	return content, nil
+}
+
+// UpdateFile 更新 IPFS 网络中的文件内容
+func (o *octopusImpl) UpdateFile(cid, newFilePath string) (string, error) {
+	// 首先删除旧文件
+	err := o.shell.Unpin(cid)
+	if err != nil {
+		return "", fmt.Errorf("failed to remove old file from IPFS: %v", err)
+	}
+
+	// 添加新文件
+	newCid, err := o.shell.Add(newFilePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to add new file to IPFS: %v", err)
+	}
+
+	return newCid, nil
+}
+
+// DeleteFile 从 IPFS 网络中删除文件
+func (o *octopusImpl) DeleteFile(cid string) error {
+	err := o.shell.Unpin(cid)
+	if err != nil {
+		return fmt.Errorf("failed to delete file from IPFS: %v", err)
+	}
+	return nil
+}
